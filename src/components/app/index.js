888419a9be5af537aefb62b23b417323613c4bf5
petermonky <wjdgustjr0554@gmail.com>
2022-06-19 21:26:13 +0800

    refactor and add navbar

---
+++ src/components/app/index.js
@@ -0,0 +1,14 @@
+import "./App.scss";
+
+import Navbar from "../navbar";
+
+function App() {
+  return (
+    <div className="app">
+      <Navbar />
+      <div className="content"></div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/navbar/index.js
@@ -0,0 +1,11 @@
+import "./Navbar.scss";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-name text-2xl">Peter</div>
+    </nav>
+  );
+};
+
+export default Navbar;
